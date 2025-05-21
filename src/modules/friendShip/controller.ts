9dd8b7a58043c0b3d33fb8c0b063cd251c1884a5
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2025-05-21 16:01:59 +0700

    controller get list , create, change state

--- src/modules/friendShip/controller.ts
+++ src/modules/friendShip/controller.ts
@@ -4,11 +4,30 @@ import { handleControllerError } from '~/utils/handle.util'
 import { CreateFriendShipInputDTO } from '~/modules/friendShip/dto/Create'
 import { ApiResponse, sendResponse } from '~/response/api.response'
 import { httpStatusCode } from '~/global/httpStatusCode'
+import { FRIENDSHIPSTATUS } from '~/entities/friendShips.entity'
+import { ChangeStatusFriendShipInputDTO } from '~/modules/friendShip/dto/ChangeStatusFriend'
+import { DeleteFriendShipInputDTO } from '~/modules/friendShip/dto/Delete'
 export class FriendShipController {
   constructor(private service: IFriendShipService) {
     this.service = service
   }
 
+  async getListSendFriendShips(req: Request, res: Response) {
+    try {
+      const { id } = req.params
+      const response = await this.service.getListSendFriendShips(Number(id))
+      sendResponse(
+        new ApiResponse({
+          data: response,
+          res: res,
+          statusCode: httpStatusCode.OK,
+          message: 'Get list send friend ship successfully'
+        })
+      )
+    } catch (error) {
+      handleControllerError(error, res)
+    }
+  }
   /**
    * @body
    *    - `senderId` (number): Id người gữi lời mời kết bạn
@@ -17,10 +36,12 @@ export class FriendShipController {
   async create(req: Request, res: Response) {
     try {
       const { senderId, receiverId } = req.body
+
       const dto = new CreateFriendShipInputDTO({
         senderId: senderId,
         receiverId: receiverId
       })
+
       const response = await this.service.create(dto)
       sendResponse(
         new ApiResponse({
@@ -28,6 +49,81 @@ export class FriendShipController {
           res: res,
           statusCode: httpStatusCode.CREATED,
           message: 'Create friend ship successfully'
+        })
+      )
+    } catch (error) {
+      handleControllerError(error, res)
+    }
+  }
+
+  /**
+   * @body
+   *    - `senderId` (number): Id người gữi lời mời kết bạn
+   *    - `receiverId` (number): Id người nhận lời mời kết bạn
+   *   */
+  async accept(req: Request, res: Response) {
+    try {
+      const { senderId, receiverId } = req.body
+      const dto = new ChangeStatusFriendShipInputDTO({
+        receiverId: receiverId,
+        senderId: senderId,
+        status: FRIENDSHIPSTATUS.ACCEPTED
+      })
+      const response = await this.service.changeStatus(dto)
+      sendResponse(
+        new ApiResponse({
+          data: response,
+          res: res,
+          statusCode: httpStatusCode.OK,
+          message: 'Accept friend ship successfully'
+        })
+      )
+    } catch (error) {
+      handleControllerError(error, res)
+    }
+  }
+
+  /**
+   * @body
+   *    - `senderId` (number): Id người gữi lời mời kết bạn
+   *    - `receiverId` (number): Id người nhận lời mời kết bạn
+   *   */
+  async reject(req: Request, res: Response) {
+    try {
+      const { senderId, receiverId } = req.body
+      const dto = new ChangeStatusFriendShipInputDTO({
+        receiverId: receiverId,
+        senderId: senderId,
+        status: FRIENDSHIPSTATUS.REJECTED
+      })
+      const response = await this.service.changeStatus(dto)
+      sendResponse(
+        new ApiResponse({
+          data: response,
+          res: res,
+          statusCode: httpStatusCode.OK,
+          message: 'Reject friend ship successfully'
+        })
+      )
+    } catch (error) {
+      handleControllerError(error, res)
+    }
+  }
+  async delete(req: Request, res: Response) {
+    try {
+      const { senderId, receiverId } = req.body
+      const response = await this.service.delete(
+        new DeleteFriendShipInputDTO({
+          receiverId: receiverId,
+          senderId: senderId
+        })
+      )
+      sendResponse(
+        new ApiResponse({
+          data: response,
+          res: res,
+          statusCode: httpStatusCode.OK,
+          message: 'Delete friend ship successfully'
         })
       )
     } catch (error) {
