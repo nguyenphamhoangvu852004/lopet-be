6c71482a70a4858d20c4281700081d8e859393cb
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2025-05-21 08:13:06 +0700

    dto

--- src/modules/post/dto/Get.ts
+++ src/modules/post/dto/Get.ts
@@ -7,9 +7,54 @@ export class GetPostOutputDTO {
   groupId?: number | null
   postType!: string
   postMedias?: PostMediaInputDTO[]
+  likeAmount!: number
   createdAt!: Date
   updatedAt!: Date | null
   constructor(data?: Partial<GetPostOutputDTO>) {
+    Object.assign(this, data)
+  }
+}
+
+export class AccountDTO {
+  id!: number
+  username!: string
+  email!: string
+  constructor(data?: Partial<AccountDTO>) {
+    Object.assign(this, data)
+  }
+}
+
+export class GetPostDetailOutputDTO {
+  postId!: number
+  accountId!: number
+  content!: string
+  groupId?: number | null
+  postType!: string
+  postMedias?: PostMediaInputDTO[]
+  likeAmount!: number
+  commentAmount!: number
+  shareAmount!: number
+  listLike!: AccountDTO[]
+  createdAt!: Date
+  updatedAt!: Date | null
+  constructor(data?: Partial<GetPostDetailOutputDTO>) {
+    Object.assign(this, data)
+  }
+}
+
+export class GetPostByAccountIdOutputDTO {
+  postId!: number
+  content!: string
+  groupId?: number | null
+  postType!: string
+  postMedias?: PostMediaInputDTO[]
+  likeAmount!: number
+  commentAmount!: number
+  shareAmount!: number
+  listLike!: AccountDTO[]
+  createdAt!: Date
+  updatedAt!: Date | null
+  constructor(data?: Partial<GetPostByAccountIdOutputDTO>) {
     Object.assign(this, data)
   }
 }
