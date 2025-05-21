5476e7618aaea8681b3f543cbd7e299ff8d76a37
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2025-05-21 16:02:49 +0700

    them route

--- src/routes/v1/friendShip.route.ts
+++ src/routes/v1/friendShip.route.ts
@@ -8,7 +8,12 @@ export const friendShipRouter = Router()
 const friendShipRepo = new FriendShipRepositoriesImpl()
 const accountRepo = new AccountRepoImpl()
 const service = new FriendShipServiceImpl(friendShipRepo, accountRepo)
-
 const controller = new FriendShipController(service)
 
+friendShipRouter.get('/:id', controller.getListSendFriendShips.bind(controller))
+
 friendShipRouter.post('/', controller.create.bind(controller))
+friendShipRouter.post('/accept', controller.accept.bind(controller))
+friendShipRouter.post('/reject', controller.reject.bind(controller))
+
+friendShipRouter.delete('/', controller.delete.bind(controller))
