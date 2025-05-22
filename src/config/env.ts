ab6fc3fa0daf7f2b167d037e1d6e3effa7a3f621
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2025-05-22 14:25:48 +0700

    config env

--- src/config/env.ts
+++ src/config/env.ts
@@ -5,6 +5,8 @@ export const environment = {
   APP_PORT: Number(process.env.APP_PORT),
   APP_HOSTNAME: process.env.APP_HOSTNAME,
 
+  DOMAIN_CORS: process.env.DOMAIN_CORS,
+
   DATABASE_HOSTNAME: process.env.DATABASE_HOST,
   DATABASE_PORT: Number(process.env.DATABASE_PORT),
   DATABASE_USERNAME: process.env.DATABASE_USERNAME,
