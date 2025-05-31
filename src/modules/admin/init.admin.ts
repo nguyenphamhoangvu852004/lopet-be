46f75fa168ba373a828424b571b0c7a155feccb7
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2025-05-31 10:51:47 +0700

    init account for ADMIN

---
+++ src/modules/admin/init.admin.ts
@@ -0,0 +1,17 @@
+import AccountRepoImpl from '~/modules/account/repositories/AccountRepoImpl'
+import AdminController from './account.controller'
+import AdminServiceImpl from './service/AdminServiceImpl'
+
+export default class InitAdmin {
+  adminController: AdminController
+  constructor() {
+    const accountRepo = new AccountRepoImpl()
+    const adminService = new AdminServiceImpl(accountRepo)
+    this.adminController = new AdminController(adminService)
+  }
+
+  async init() {
+    await this.adminController.createAccountAdmin()
+    await this.adminController.setRoleToAdmin()
+  }
+}
