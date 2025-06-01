70e9f42af05637fdaaff3b9bb29903abfbf55a2b
nguyenphamhoangvu852004 <nguyenphamhoangvu852004@gmail.com>
2025-06-01 14:38:09 +0700

    config test module

--- jest.config.ts
+++ jest.config.ts
@@ -1,17 +1,11 @@
 import type { Config } from 'jest'
-import { createDefaultPreset } from 'ts-jest'
-
-const tsJestPreset = createDefaultPreset()
 
 const config: Config = {
-  ...tsJestPreset,
+  preset: 'ts-jest',
   testEnvironment: 'node',
-  transform: {
-    ...tsJestPreset.transform
-  },
   testMatch: ['**/__tests__/**/*.test.ts'],
   moduleFileExtensions: ['ts', 'js', 'json'],
-  roots: ['<rootDir>/src'],
+  roots: ['<rootDir>/'],
   moduleNameMapper: {
     '^~/(.*)$': '<rootDir>/src/$1',
     '^src/(.*)$': '<rootDir>/src/$1'
@@ -19,3 +13,25 @@ const config: Config = {
 }
 
 export default config
+
+// import type { Config } from 'jest'
+// import { createDefaultPreset } from 'ts-jest'
+
+// const tsJestPreset = createDefaultPreset()
+
+// const config: Config = {
+//   ...tsJestPreset,
+//   testEnvironment: 'node',
+//   transform: {
+//     ...tsJestPreset.transform
+//   },
+//   testMatch: ['**/__tests__/**/*.test.ts'],
+//   moduleFileExtensions: ['ts', 'js', 'json'],
+//   roots: ['<rootDir>/'],
+//   moduleNameMapper: {
+//     '^~/(.*)$': '<rootDir>/src/$1',
+//     '^src/(.*)$': '<rootDir>/src/$1'
+//   }
+// }
+
+// export default config
